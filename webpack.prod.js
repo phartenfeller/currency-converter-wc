9f5da3d5ddb77e66efb46957b4b43e1543d4a254
phartenfeller <pharti@hotmail.de>
2021-03-09 21:29:10 +0100

    renamed webpack configs and added terser

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'currency-converter.js'),
   optimization: {
     minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'CurrencyConverter.js',
+    filename: 'index.js',
   },
 };
